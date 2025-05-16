2db357c241f55813e0c844d618b44a72297f6b5b
wireside <wiresifed@gmail.com>
2025-05-17 00:38:43 +0300

    added middlewares

--- index.js
+++ index.js
@@ -13,19 +13,21 @@ app.get('/', (req, res) => {
 
 app.get('/hello', (req, res) => {
 	// res.set('Content-Type', 'text/plain');
-	res.cookie('token', 'asdadade32e4c', {
-		domain: '',
-		path: '/',
-		secure: true,
-		expires: 6000000,
-	})
-	res.clearCookie('token') // when logout
-	res.type('application/json');
-	res.location('/hello')
-	res.links({
-		next: 'dadasd',
-	});
-	res.append('Warning', 'custom-http-response-header');
+	// res.cookie('token', 'asdadade32e4c', {
+	// 	domain: '',
+	// 	path: '/',
+	// 	secure: true,
+	// 	expires: 6000000,
+	// })
+	// res.clearCookie('token') // when logout
+	// res.type('application/json');
+	// res.location('/hello')
+	// res.links({
+	// 	next: 'dadasd',
+	// });
+	// res.append('Warning', 'custom-http-response-header');
+	throw new Error('Error!!!');
+	
 	res.send('Привет!');
 });
 
@@ -34,7 +36,13 @@ app.get('/nothing', (req, res) => {
 })
 
 app.get('/never', (req, res) => {
-	res.end();
+	res.end()
+})
+
+// error handler middleware should be after all app.use() calls
+app.use((err, req, res, next) => {
+	console.log(err.message);
+	res.status(401).send(err.message)
 })
 
 app.listen(port, () => {
--- users/users.js
+++ users/users.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const userRouter = express.Router();
 
+userRouter.use((req, res, next) => {
+	console.log('Handled by middleware:', new Date(Date.now()));
+	next();
+})
+
 userRouter.post('/login', (req, res) => {
 	res.send('login')
 })
